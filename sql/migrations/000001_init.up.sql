227203b3fefd2dd1321e4bd233f0f29bbbc49fc7
yogarn <radityanalaa@gmail.com>
2025-03-11 08:16:57 +0700

    `chore:` Add unique constraints to db

--- sql/migrations/000001_init.up.sql
+++ sql/migrations/000001_init.up.sql
@@ -1,14 +1,14 @@
 CREATE TABLE users (
     id varchar(36) NOT NULL PRIMARY KEY,
-    username varchar(36) NOT NULL,
-    email varchar(255),
-    password varchar(60),
+    username varchar(36) NOT NULL UNIQUE,
+    email varchar(255) NOT NULL UNIQUE,
+    password varchar(60) NOT NULL,
     role_id integer NOT NULL DEFAULT 0
 );
 
 CREATE TABLE roles (
     id INTEGER NOT NULL PRIMARY KEY,
-    role varchar(36) NOT NULL
+    role varchar(36) NOT NULL UNIQUE
 );
 
 INSERT INTO roles (id, role) VALUES 
@@ -17,20 +17,19 @@ INSERT INTO roles (id, role) VALUES
 
 CREATE TABLE sessions (
     user_id varchar(36),
-    token varchar(255),
+    token varchar(255) UNIQUE,
     ip_address varchar(16),
     expires_at timestamp,
     user_agent varchar(255),
-    device_id varchar(255), 
+    device_id varchar(255) UNIQUE, 
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE books (
-    book_id VARCHAR(36) PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+    id VARCHAR(36) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL UNIQUE,
     description TEXT NOT NULL,
     author VARCHAR(255) NOT NULL,
     release_date DATE NOT NULL,
     price DECIMAL NOT NULL CHECK(price > 1000)
 );
-

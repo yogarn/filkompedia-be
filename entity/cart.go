57c7a025e94178bc5abbb04cff75d3ee45121eb8
agungaryansyah <agungaryansyah2006@gmail.com>
2025-03-17 14:41:21 +0700

    feat: add create book:

--- entity/cart.go
+++ entity/cart.go
@@ -3,7 +3,7 @@ package entity
 import "github.com/google/uuid"
 
 type Cart struct {
-	CartId uuid.UUID `json:"cart_id" db:"cart_id"`
+	Id     uuid.UUID `json:"cart_id" db:"cart_id"`
 	UserId uuid.UUID `json:"user_id" db:"user_id"`
 	BookId uuid.UUID `json:"book_id" db:"book_id"`
 	Amount int       `json:"amount" db:"amount"`
--- internal/service/cart.go
+++ internal/service/cart.go
@@ -1,16 +1,41 @@
 package service
 
-import (
-	"github.com/jmoiron/sqlx"
-	"github.com/yogarn/filkompedia-be/internal/repository"
-)
+// import (
+// 	"github.com/google/uuid"
+// 	"github.com/yogarn/filkompedia-be/entity"
+// 	"github.com/yogarn/filkompedia-be/internal/repository"
+// 	"github.com/yogarn/filkompedia-be/model"
+// )
 
-type ICartService interface {
-}
+// type ICartService interface {
+// 	GetUserCart(carts *[]entity.Cart, UserId uuid.UUID) error
+// 	GetCart(param *model.CartParam, cart *entity.Cart) error
+// 	AddToCart(add model.AddToCart) error
+// 	RemoveFromCart(param *model.CartParam) error
+// }
 
-type CartService struct {
-}
+// type CartService struct {
+// 	cartRepo repository.ICartRepository
+// }
 
-func NewCartService(db *sqlx.DB, cartRepo repository.ICartRepository) {
+// func NewCartService(cartRepo repository.ICartRepository) ICartService {
+// 	return &CartService{
+// 		cartRepo: cartRepo,
+// 	}
+// }
 
-}
+// func (s *CartService) GetUserCart(carts *[]entity.Cart, UserId uuid.UUID) error {
+// 	return s.cartRepo.GetUserCart(carts, UserId)
+// }
+
+// func (s *CartService) GetCart(param *model.CartParam, cart *entity.Cart) error {
+
+// }
+
+// func (s *CartService) AddToCart(add model.AddToCart) error {
+
+// }
+
+// func (s *CartService) RemoveFromCart(param *model.CartParam) error {
+
+// }

4d009b1d9697be16a919ae42919c21652e9567e1
yogarn <radityanalaa@gmail.com>
2025-03-23 14:43:40 +0700

    `chore:` Update migration schema

--- sql/migrations/000005_add_comments_table.up.sql
+++ sql/migrations/000005_add_comments_table.up.sql
@@ -1,8 +1,10 @@
-CREATE TABLE carts (
+CREATE TABLE comments (
     id VARCHAR(36) PRIMARY KEY,
     user_id VARCHAR(36) NOT NULL,
     book_id VARCHAR(36) NOT NULL,
-    amount INTEGER NOT NULL CHECK(amount > 0),
+    comments TEXT NOT NULL,
+    rating INT NOT NULL
+
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE
 );

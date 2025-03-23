cbe17558c6eb5961c3d1e4efa8e82c0db3ed944a
agungaryansyah <agungaryansyah2006@gmail.com>
2025-03-23 13:35:13 +0700

    fix: fix sql migrations for checkouts

--- sql/migrations/000004_add_checkouts_table.up.sql
+++ sql/migrations/000004_add_checkouts_table.up.sql
@@ -1,4 +1,8 @@
-DROP TABLE IF EXISTS checkouts;
+CREATE TABLE checkouts (
+    id VARCHAR(36) PRIMARY KEY,
+    user_id varchar(36) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
-ALTER TABLE carts ADD COLUMN checkout_id UUID;
+ALTER TABLE carts ADD COLUMN checkout_id VARCHAR(36);
 ALTER TABLE carts ADD FOREIGN KEY (checkout_id) REFERENCES checkouts(id);

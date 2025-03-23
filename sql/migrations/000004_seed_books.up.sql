5efc93cb1017334c4354b8b7de2534f8623e71bf
yogarn <radityanalaa@gmail.com>
2025-03-23 12:19:26 +0700

    `chore:` Add books seeder

---
+++ sql/migrations/000004_seed_books.up.sql
@@ -0,0 +1,150 @@
+INSERT INTO
+    books (
+        id,
+        title,
+        description,
+        introduction,
+        author,
+        release_date,
+        image,
+        file,
+        price
+    )
+VALUES
+    (
+        '79fd3225-bfa7-404d-a233-3a73b27cefa4',
+        'The Pragmatic Programmer',
+        'A classic book on software development best practices.',
+        'Ward Cunningham Straight from the programming trenches, The Pragmatic Programmer cuts through the increasing specialization and technicalities of modern software development to examine the core process--taking a requirement and producing working, maintainable code that delights its users. It covers topics ranging from personal responsibility and career development to architectural techniques for keeping your code flexible and easy to adapt and reuse. Read this book, and you''ll learn how to Fight software rot; Avoid the trap of duplicating knowledge; Write flexible, dynamic, and adaptable code; Avoid programming by coincidence; Bullet-proof your code with contracts, assertions, and exceptions; Capture real requirements; Test ruthlessly and effectively; Delight your users; Build teams of pragmatic programmers; and Make your developments more precise with automation. Written as a series of self-contained sections and filled with entertaining anecdotes, thoughtful examples, and interesting analogies, The Pragmatic Programmer illustrates the best practices and major pitfalls of many different aspects of software development. Whether you''re a new coder, an experienced program. ',
+        'Andrew Hunt & David Thomas',
+        '1999-10-20',
+        'https://covers.openlibrary.org/b/id/135412-L.jpg',
+        '',
+        1500.00
+    ),
+    (
+        'b56f251c-a31b-47b2-b38e-8e7147929476',
+        'Clean Code',
+        'A guide to writing clean, readable, and maintainable code.',
+        'Even bad code can function. But if code isn''t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn''t have to be that way.
+            Noted software expert Robert C. Martin, presents a revolutionary paradigm with Clean Code: A Handbook of Agile Software Craftsmanship. Martin, who has helped bring agile principles from a practitioner''s point of view to tens of thousands of programmers, has teamed up with his colleagues from Object Mentor to distill their best agile practice of cleaning code “on the fly” into a book that will instill within you the values of software craftsman, and make you a better programmer―but only if you work at it.
+            What kind of work will you be doing? You''ll be reading code―lots of code. And you will be challenged to think about what''s right about that code, and what''s wrong with it. More importantly you will be challenged to reassess your professional values and your commitment to your craft.',
+        'Robert C. Martin',
+        '2008-08-01',
+        'https://covers.openlibrary.org/b/id/14840846-M.jpg',
+        '',
+        1800.00
+    ),
+    (
+        'ff50722c-5de9-44d5-ad16-9e44d040152d',
+        'Design Patterns',
+        'A book that introduced the concept of software design patterns.',
+        'Four software designers present a catalog of simple and succinct solutions to commonly occurring design problems, using Smalltalk and C++ in example code. These 23 patterns allow designers to create more flexible, elegant, and ultimately reusable designs without having to rediscover the design solutions themselves. The authors begin by describing what patterns are and how they can help you design object-oriented software. They go on to systematically name, explain, evaluate, and catalog recurring designs in object-oriented systems.',
+        'Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides',
+        '1994-10-21',
+        '',
+        '',
+        2200.00
+    ),
+    (
+        '853a47d6-29a7-4327-8894-0c34aab1076f',
+        'Refactoring',
+        'Techniques for improving code quality and structure.',
+        'For more than twenty years, experienced programmers worldwide have relied on Martin Fowler''s Refactoring to improve the design of existing code and to enhance software maintainability, as well as to make existing code easier to understand.
+            This eagerly awaited new edition has been fully updated to reflect crucial changes in the programming landscape. Refactoring, Second Edition, features an updated catalog of refactoring''s and includes JavaScript code examples, as well as new functional examples that demonstrate refactoring without classes.',
+        'Martin Fowler',
+        '1999-07-08',
+        'https://covers.openlibrary.org/b/id/7087623-M.jpg',
+        '',
+        2000.00
+    ),
+    (
+        '4001e6db-91f0-4074-a909-0cea31303a54',
+        'The Mythical Man-Month',
+        'Essays on software engineering and project management.',
+        'Classic text on the human side of software engineering, containing essays on the management of software teams, projections about how computer languages and tools will evolve, and philosophical speculation. Unlike most other books about computing, Brooks'' work has been remarkably enduring, remaining in print for at least four decades. The book is most famous for its statement of Brooks'' Law: "adding manpower to a late software project makes it later".',
+        'Frederick P. Brooks Jr.',
+        '1975-08-01',
+        'https://covers.openlibrary.org/b/id/10653425-M.jpg',
+        '',
+        1700.00
+    ),
+    (
+        'c0c01901-6504-422a-8448-08a0813a0a79',
+        'Code Complete',
+        'A practical handbook for software construction.',
+        'Take a strategic approach to software construction—and produce superior products—with this fully updated edition of Steve McConnell'' s critically praised
+        and award - winning guide to software development best practices.Widely considered one of the best practical guides to programming,
+        Steve McConnell''s original CODE COMPLETE has been helping developers write better software for more than a decade. Now this classic book has been fully updated and revised with leading-edge practices—and hundreds of new code samples—illustrating the art and science of software construction. Capturing the body of knowledge available from research, academia, and everyday commercial practice, McConnell synthesizes the most effective techniques and must-know principles into clear, pragmatic guidance. No matter what your experience level, development environment, or project size, this book will inform and stimulate your thinking—and help you build the highest quality code.Discover the timeless techniques and strategies that help you:Design for minimum complexity and maximum creativityReap the benefits of collaborative developmentApply defensive programming techniques to reduce and flush out errorsExploit opportunities to refactor—or evolve—code, and do it safelyUse construction practices that are right-weight for your projectDebug problems quickly and effectivelyResolve critical construction issues early and correctlyBuild quality into the beginning, middle, and end of your project',
+        'Steve McConnell',
+        '1993-06-09',
+        'https://covers.openlibrary.org/b/id/8085500-M.jpg',
+        '',
+        1900.00
+    ),
+    (
+        'ae9c2fdc-7a4d-4310-b66c-8d3d2bd9625b',
+        'JavaScript: The Good Parts',
+        'Exploring the best features of JavaScript.',
+        'Covers the bits of Javascript that are worth using, and how to use them, as well as the bits that should be avoided.',
+        'Douglas Crockford',
+        '2008-05-15',
+        'https://covers.openlibrary.org/b/id/9245523-M.jpg',
+        '',
+        1400.00
+    ),
+    (
+        '8fd9bbdb-12fa-43c3-83f5-6125ab3fee9b',
+        'The Clean Coder',
+        'A code of conduct for professional programmers.',
+        'In the Clean Coder: A Code of Conduct for Professional Programmers, legendary software expert Robert C Martin introduces the disciplines, techniques, tools, and practices of true software craftsmanship.',
+        'Robert C. Martin',
+        '2011-05-13',
+        'https://covers.openlibrary.org/b/id/11311437-M.jpg',
+        '',
+        1600.00
+    ),
+    (
+        '8e50d42f-674e-42c3-b990-7ec90865e8b9',
+        'Soft Skills',
+        'The software developer''s life manual.',
+        'Soft Skills: The software developer''s life manual is a unique guide, offering techniques and practices for a more satisfying life as a professional software developer. In it, developer and life coach John Sonmez addresses a wide range of important "soft" topics, from career and productivity to personal finance and investing, and even fitness and relationships, all from a developer-centric viewpoint.',
+        'John Sonmez',
+        '2014-12-28',
+        'https://covers.openlibrary.org/b/id/12033351-M.jpg',
+        '',
+        1700.00
+    ),
+    (
+        'c0e43c0a-12d2-4074-8909-57e966a5ca7e',
+        'Introduction to Algorithms',
+        'A comprehensive guide to algorithms.',
+        'The first edition became a widely used text in universities worldwide as well as the standard reference for professionals. The second edition featured new chapters on the role of algorithms, probabilistic analysis and randomized algorithms, and linear programming. The third edition has been revised and updated throughout. It includes two completely new chapters, on van Emde Boas trees and multithreaded algorithms, substantial additions to the chapter on recurrence (now called "Divide-and-Conquer"), and an appendix on matrices. It features improved treatment of dynamic programming and greedy algorithms and a new notion of edge-based flow in the material on flow networks. Many exercises and problems have been added for this edition. The international paperback edition is no longer available; the hardcover is available worldwide.',
+        'Thomas H. Cormen',
+        '2009-07-31',
+        'https://covers.openlibrary.org/b/id/6709987-M.jpg',
+        '',
+        2500.00
+    ),
+    (
+        'b351d2b2-6c99-4f74-a011-938d6d499cae',
+        'Eloquent JavaScript',
+        'A modern introduction to JavaScript.',
+        'JavaScript lies at the heart of almost every modern web application, from social apps like Twitter to browser-based game frameworks like Phaser and Babylon. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications. This much anticipated and thoroughly revised third edition of Eloquent JavaScript dives deep into the JavaScript language to show you how to write beautiful, effective code. It has been updated to reflect the current state of Java¬Script and web browsers and includes brand-new material on features like class notation, arrow functions, iterators, async functions, template strings, and block scope. A host of new exercises have also been added to test your skills and keep you on track. As with previous editions, Haverbeke continues to teach through extensive examples and immerses you in code from the start, while exercises and full-chapter projects give you hands-on experience with writing your own programs. You start by learning the basic structure of the JavaScript language as well as control structures, functions, and data structures to help you write basic programs. Then you''ll learn about error handling and bug fixing, modularity, and asynchronous programming before moving on to web browsers and how JavaScript is used to program them.',
+        'Marijn Haverbeke',
+        '2018-12-04',
+        'https://covers.openlibrary.org/b/id/12995869-M.jpg',
+        '',
+        1600.00
+    ),
+    (
+        '7c83db44-e06e-435a-a3c3-0d176fd83087',
+        'Domain-Driven Design',
+        'Tackling complexity in software development.',
+        'Building software is harder than ever. As a developer, you not only have to chase ever-changing technological trends but also need to understand the business domains behind the software. This practical book provides you with a set of core patterns, principles, and practices for analyzing business domains, understanding business strategy, and, most importantly, aligning software design with its business needs.',
+        'Eric Evans',
+        '2003-08-30',
+        'https://covers.openlibrary.org/b/id/7375231-M.jpg',
+        '',
+        2400.00
+    );

d2b3fb70deb9022e0127a8f70eb53f4c0cd20f56
yogarn <radityanalaa@gmail.com>
2025-03-30 08:36:38 +0700

    `fix:` Add missing cors method

--- pkg/config/fiber.go
+++ pkg/config/fiber.go
@@ -17,7 +17,7 @@ func StartFiber() *fiber.App {
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     "http://localhost:5173, https://filkompedia.yogarn.my.id, https://api.sandbox.midtrans.com",
 		AllowCredentials: true,
-		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS,PATCH",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		ExposeHeaders:    "Set-Cookie",
 	}))
